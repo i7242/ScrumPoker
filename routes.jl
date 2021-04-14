3fe37b0ba27f72e06a383edf27768dc61e9038c0
i7242 <xingyuyan@outlook.com>
2021-04-14 01:46:14 -0400

    save alias in session storage, then send with request

--- routes.jl
+++ routes.jl
@@ -4,42 +4,24 @@ using Genie.Assets
 
 Genie.config.websockets_server = true
 
-login_users = Dict{String, String}()
-
-Sessions.init()
-
-function validate_and_redirect(sid::String)
-    if haskey(login_users, sid)
-        redirect(:scrum_url)
-    else
-        redirect(:login_url)
-    end
-end
-
-route("/") do
-    sid = Sessions.id(request())
-    validate_and_redirect(sid)
-end
-
 route("/login", named=:login_url) do
     html(:login, :login, layout=:scrumpoker)
 end
 
-route("/login/update", named=:update_user_info_url) do 
-    alias = get(@params, :alias, "")
-    if length(strip(alias)) > 0
-        sid = Sessions.id()
-        sess, resp = Sessions.start(sid, request(), getresponse())
-        login_users[sid] = alias
-        redirect(:scrum_url)
-    else
-        redirect(:login_url)
-    end
+route("/scrum", named=:scrum_url) do
+    Assets.channels_support() *
+    """
+    <h1 id=alias></h1>
+    <script>
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const alias = urlParams.get('alias');
+    document.getElementById('alias').innerHTML = alias;
+    sessionStorage.setItem("scrum_alias", alias);
+    </script>
+    """
 end
 
-route("/scrum", named=:scrum_url) do
-    # test to start a channel
-    Assets.channels_support()
-
-    Genie.WebChannels.broadcast("__", string(Genie.WebChannels.connected_clients()))
+channel("/__/echo") do
+    "$(getpayload(:alias, "none"))"
 end
